8b1ea167416add2097087f06cd18a4bd6f758a4f
wally2online <>
2021-05-28 15:13:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("\nWelcome to the Name Generator, OF YOUR NEW MUSIC ROCK BAND.")
+street = input("\nWhat's name of the city you grew up in?\n")
+pet = input("\nWhat's your pet's name?\n")
+print("\nYour band name could be \n" + street + " " + pet+"\n")
